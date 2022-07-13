6147bb40de4019052d434fed0901337df6b459a9
anthonydouc <anthonydouc@hotmail.com>
2022-07-13 16:55:12 +1000

    add charting

---
+++ charting_results.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from bokeh.plotting import figure
+from bokeh.io import show
+from bokeh.layouts import row
+
+from data import get_data, get_node_positions, load_submission
+
+from algorithms import find_tsp_path, calc_path_distance_prime#, get_edges_from_path
+
+def create_bk_plot(**kwargs):
+    fig = figure(**kwargs)
+    fig = plot_format(fig)
+    return fig
+
+def plot_format(p):
+
+    p.xaxis.visible = False
+    p.yaxis.visible = False
+    p.xgrid.visible = False
+    p.ygrid.visible = False
+
+    p.outline_line_color = None
+    p.border_fill_color = None
+    p.border_fill_alpha = 0
+
+    return p
+
+cities, centers = get_data(used_saved=True, ncluster=500)
+
+pX, pY = get_node_positions(cities)
+
+path = load_submission('test')
+
+# TODO summarise the path length distribution. May need to be seperated 
+
+f1 = create_bk_plot(output_backend='webgl',
+                    height=700, width=1000,
+                    x_range=(min(pX), max(pX)),
+                    y_range=(min(pY), max(pY)))
+
+
+f1.line(x=pX[path], y=pY[path])
+f1.scatter(x=[pX[0]], y=[pY[0]], size=40 , color='#fb8500', marker='star')
+
+#f1.scatter(x=pX, y=pY, size=0.3, color='#0077b6')
+
+
+show(f1)
+
+
+
